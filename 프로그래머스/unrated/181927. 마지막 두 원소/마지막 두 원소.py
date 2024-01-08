348a7c07a01143daa85aa84b143db45c7d9c98c9
phyuna0525 <128463899+phyuna0525@users.noreply.github.com>
2024-01-08 09:37:15 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    a = len(num_list)
+    if num_list[a-1] > num_list[a-2]:
+        num_list.append(num_list[a-1] - num_list[a-2])
+    else:
+        num_list.append(num_list[a-1] * 2)
+    return num_list
