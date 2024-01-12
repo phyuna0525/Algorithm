edcc05314cb4a1b257240aaaabf111e85189b902
phyuna0525 <128463899+phyuna0525@users.noreply.github.com>
2024-01-12 22:03:20 +0900

    [level Lv.0] Title: 문자열 여러 번 뒤집기, Time: 1.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -0,0 +1,6 @@
+def solution(my_string, queries):
+    for query in queries:
+        start, end = query
+        my_string = my_string[:start] + my_string[start:end+1][::-1] + my_string[end+1:]
+    return my_string
+
